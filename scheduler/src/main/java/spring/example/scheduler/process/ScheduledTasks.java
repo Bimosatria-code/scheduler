939b842b85491c99d323d075d2b0895f2fc461fc
Bimosatria-code <bimosatria6@gmail.com>
2023-07-27 17:11:32 +0700

    first commit

---
+++ scheduler/src/main/java/spring/example/scheduler/process/ScheduledTasks.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spring.example.scheduler.process;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+	private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+	@Scheduled(fixedRate = 10000)
+	public void reportCurrentTime() {
+		log.info("Springboot scheduler task. The time is now {}", dateFormat.format(new Date()));
+	}
+}
